1df37812d7fb2a543920ae9219d7958176099804
Jocelyn Remington <jocelynrem@gmail.com>
2024-06-18 13:28:06 -0400

    chore: Update MongoDB connection options and allowed origins in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,27 +9,20 @@ const app = express();
 
 const mongoUri = process.env.MONGODB_URI;
 
-mongoose.connect(mongoUri)
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error('MongoDB connection error:', err));
 
-const allowedOrigins = ['http://localhost:3000', 'https://booknav-sepia.vercel.app'];
+const allowedOrigins = ['http://localhost:3000', 'https://booknav-sepia.vercel.app/'];
 
 app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-    optionsSuccessStatus: 204
+    origin: allowedOrigins,
 }));
 
 app.use(express.json());
-
 app.use('/api/books', bookRouter);
 
 module.exports = app;
