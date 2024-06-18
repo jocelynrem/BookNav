d7d11821f8a4fafd1d7e1d3bc4e8d143b113e864
Jocelyn Remington <jocelynrem@gmail.com>
2024-06-18 16:04:36 -0400

    chore: Update allowed origins in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,10 +13,17 @@ mongoose.connect(mongoUri)
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error('MongoDB connection error:', err));
 
-const allowedOrigins = ['http://localhost:3000', 'https://booknav-sepia.vercel.app'];
+const allowedOrigins = ['http://localhost:3000', 'http://localhost:3001', 'https://booknav-sepia.vercel.app'];
 
 app.use(cors({
-    origin: allowedOrigins,
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'The CORS policy for this site does not allow access from the specified origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
 }));
 
 app.use(express.json());
