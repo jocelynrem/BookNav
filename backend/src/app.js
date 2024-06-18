fa0329c81bf5bdf67f1c5abff69414f0b41f82de
Jocelyn Remington <jocelynrem@gmail.com>
2024-06-18 15:52:14 -0400

    chore: Update MongoDB connection in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,10 @@ const app = express();
 
 const mongoUri = process.env.MONGODB_URI;
 
-mongoose.connect(mongoUri)
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error('MongoDB connection error:', err));
 
