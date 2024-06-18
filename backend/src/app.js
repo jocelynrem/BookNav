7436cb0ede86c542e0b7c666e2bdb30c122c68be
Jocelyn Remington <jocelynrem@gmail.com>
2024-06-18 16:54:31 -0400

    chore: Update app.js to listen on the provided port

--- backend/src/app.js
+++ backend/src/app.js
@@ -29,4 +29,8 @@ app.use(cors({
 app.use(express.json());
 app.use('/api/books', bookRouter);
 
-module.exports = app;
+// Listen to the port provided by Heroku
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
