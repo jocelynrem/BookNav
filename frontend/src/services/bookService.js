40bf30494bda61ae0eb768b02b0cfe95354cd72a
Jocelyn Remington <jocelynrem@gmail.com>
2024-07-16 21:50:43 -0400

    more logging

--- frontend/src/services/bookService.js
+++ frontend/src/services/bookService.js
@@ -7,6 +7,14 @@ if (!apiUrl) {
 console.log('Current API URL:', apiUrl);
 console.log('NODE_ENV:', process.env.NODE_ENV);
 
+if (process.env.NODE_ENV === 'development') {
+    console.log('Running in development mode');
+} else if (process.env.NODE_ENV === 'production') {
+    console.log('Running in production mode');
+} else {
+    console.log('Running in unknown mode:', process.env.NODE_ENV);
+}
+
 function formatDate(dateString) {
     const date = new Date(dateString);
     const year = date.getFullYear();
