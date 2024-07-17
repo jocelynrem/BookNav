b3eb0ca5d335d337818ffcfbe0097fcd07641e8d
Jocelyn Remington <jocelynrem@gmail.com>
2024-07-16 21:18:54 -0400

    added logging to debug

--- frontend/src/services/bookService.js
+++ frontend/src/services/bookService.js
@@ -1,7 +1,12 @@
 const apiUrl = process.env.NODE_ENV === 'production'
     ? process.env.REACT_APP_PROD_API_URL
-    : process.env.REACT_APP_DEV_API_URL;
+    : process.env.NODE_ENV === 'development'
+        ? process.env.REACT_APP_DEV_API_URL
+        : process.env.REACT_APP_API_URL;
 
+console.log('NODE_ENV:', process.env.NODE_ENV);
+console.log('REACT_APP_PROD_API_URL:', process.env.REACT_APP_PROD_API_URL);
+console.log('REACT_APP_DEV_API_URL:', process.env.REACT_APP_DEV_API_URL);
 console.log('Current API URL:', apiUrl);
 
 function formatDate(dateString) {
